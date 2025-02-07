c6b35540156e1d9197946c9bec8d70ec1441835d
hawi3075 <hawig3266@gmail.com>
2025-02-07 12:26:14 +0300

    Create Police Recruits.py

---
+++ Police Recruits.py
@@ -0,0 +1,21 @@
+def count_untreated_crimes(events):
+    available_officers = 0
+    untreated_crimes = 0
+
+    for event in events:
+        if event == -1:
+            if available_officers > 0:
+                available_officers -= 1
+            else:
+                untreated_crimes += 1
+        else:
+            available_officers += event
+
+    return untreated_crimes
+
+
+n = int(input())
+events = list(map(int, input().split()))
+
+
+print(count_untreated_crimes(events))
